f111d50d865395f7f41c7ea5710e1af9ec2f514d
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-06-13 20:37:03 +0300

    use of for loops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 // //Comparison Operators
 
 //1 Equals to (==)
@@ -336,11 +336,29 @@ while (numberOfBottles >= 1 ) {
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
 
+*/
 
 //Control statements For Loops
+function fibanacciGenerator (n) {
 
+      var output = [];
+      if (n === 1){
+            output = [0];
+      }else if (n === 2 ){
+            output = [0, 1];
+      }else {
+            output = [0, 1];
+            for (var i = 2; i < n; i++){
+           
+            output.push(output[output.length - 2] + output[output.length - 1]);
+          }
+      }
+ 
 
-
+            return output;
+}
+output = fibanacciGenerator (15)
+console.log(output)
 
 
 
